00cfccecdbb6745b520995833c6875eace976ddf
Wei Lun <chanweilun2003@gmail.com>
2025-08-17 15:21:13 +0800

    Add feature add and list

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Atlas {
@@ -14,12 +16,12 @@ public class Atlas {
                 " =     =       =       ========   =     =     =====  \n";
         System.out.println(logo);
 
-
-
         String welcomeMessage = "Hello! I'm Atlas\nWhat can I do for you?";
         System.out.println(PARTITION + welcomeMessage + PARTITION);
 
-        echoUserCommands();
+//        echoUserCommands();
+
+        addAndList();
 
         String exitMessage = "Bye. Hope to see you again soon!";
         System.out.println(PARTITION + exitMessage + PARTITION);
@@ -32,6 +34,27 @@ public class Atlas {
             if (command.equalsIgnoreCase("bye"))
                 break;
             System.out.println(PARTITION + command + PARTITION);
+        }
+    }
+
+    private static void addAndList() {
+        Scanner sc = new Scanner(System.in);
+        List<String> todoList = new ArrayList<>();
+        while (true) {
+            String command = sc.nextLine().trim();
+            if (command.equalsIgnoreCase("list")) {
+                System.out.print(PARTITION);
+                for (int i = 0; i < todoList.size(); i++) {
+                    System.out.println((i+1) + ". "
+                            + todoList.get(i));
+                }
+                System.out.print(PARTITION);
+            } else if (command.equalsIgnoreCase("bye")) {
+                break;
+            } else {
+                todoList.add(command);
+                System.out.println(PARTITION + "added: " + command + PARTITION);
+            }
         }
     }
 }
